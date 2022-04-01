5c543d0af3dfbdeb74c7bb1b6bceb97f0ba6276b
Lukasz Wisniewski <luke.cherrish@gmail.com>
2022-04-01 10:16:03 +0200

    setCustomAdvertisingId

--- android/src/main/kotlin/com/amplitude/amplitude_flutter/AmplitudeFlutterPlugin.kt
+++ android/src/main/kotlin/com/amplitude/amplitude_flutter/AmplitudeFlutterPlugin.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Context
 import com.amplitude.api.Amplitude
 import com.amplitude.api.AmplitudeServerZone
+import com.amplitude.api.DeviceInfo
 import com.amplitude.api.Identify
 import com.amplitude.api.Revenue
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -227,6 +228,11 @@ class AmplitudeFlutterPlugin : FlutterPlugin, MethodCallHandler {
                 val client = Amplitude.getInstance(instanceName)
                 client.setMinTimeBetweenSessionsMillis(json.getLong("timeInMillis"))
                 result.success("setMinTimeBetweenSessionsMillis called..")
+            }
+
+            "setCustomAdvertisingId" -> {
+                DeviceInfo.customAdvertisingId = json.getString("advertisingId");
+                result.success("setCustomAdvertisingId called..")
             }
 
             "setServerZone" -> {
